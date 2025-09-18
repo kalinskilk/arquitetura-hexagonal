e39a3acd2df8c97548fc5d2a525ebefbc2a9db91
Kalinski <lucascunha@xpert.com.br>
2025-09-18 16:52:33 -0300

    aula: criando dto

---
+++ adapters/dto/product.go
@@ -0,0 +1,26 @@
+package dto
+
+import (
+	"github.com/kalinskilk/arquitetura-hexagonal/application"
+)
+
+type Product struct{
+	ID string `json:"id"`
+	Name string `json:"name"`
+	Price float64 `json:"price"` 
+	Status string `json:"status"`
+}
+
+func (p *Product) Bind(product *application.Product)(*application.Product,error){
+	if p.id != ""{
+		product.ID = p.ID
+	}
+	product.Name = p.Name
+	product.Price = p.Price
+	product.Status = p.Status
+	_, err := product.isValid()
+	if err !=nil{
+		return &application.Product{},err
+	}
+	return product, nil
+}
